8d6873422be738a3f42367b1063fc20ebb132fcb
udu3324 <47045986+udu3324@users.noreply.github.com>
2022-08-25 18:40:45 -0500

    Fix Upload Image not working on IOS

--- src/Start.js
+++ src/Start.js
@@ -4,44 +4,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUpload } from '@fortawesome/free-solid-svg-icons'
 import { setImage } from ".";
 
-function openFileDialog() {
-    const inputElement = document.createElement("input");
-
-    inputElement.type = "file";
-    inputElement.accept = "image/png, image/jpeg";
-
-    inputElement.addEventListener("change", function () {
-        const fileList = this.files;
-
-        for (let i = 0, numFiles = fileList.length; i < numFiles; i++) {
-            const file = fileList[i];
-            var reader = new FileReader()
-            reader.onload = function (base64) {
-                //base64.target.result
-                setImage(base64.target.result)
-            }
-            reader.readAsDataURL(file);
-        }
-    })
-
-    inputElement.dispatchEvent(new MouseEvent("click"));
-}
-
 class Start extends React.Component {
     constructor(props) {
         super(props);
-        this.openFileExplorer = this.openFileExplorer.bind(this);
+
+        this.simulateClick = this.simulateClick.bind(this);
+        this.getFile = this.getFile.bind(this);
     }
 
-    openFileExplorer() {
-        openFileDialog()
+    simulateClick() {
+        document.getElementById('upload-file').click();
+    }
+
+    getFile() {
+        const inputElement = document.getElementById('upload-file');
+
+        inputElement.addEventListener("change", function () {
+            const fileList = this.files;
+
+            for (let i = 0, numFiles = fileList.length; i < numFiles; i++) {
+                const file = fileList[i];
+                var reader = new FileReader()
+                reader.onload = function (base64) {
+                    //base64.target.result
+                    setImage(base64.target.result)
+                }
+                reader.readAsDataURL(file);
+            }
+        })
     }
 
     render() {
         return (
             <div id="start">
                 <div className="start-text-div"><span className="start-text">PNGskin is a Minecraft Skin Generator that turns images into skins.</span></div>
-                <button onClick={this.openFileExplorer} className="upload-image-button"><FontAwesomeIcon icon={faUpload} /> Upload Image</button>
+                <button onClick={this.simulateClick} className="upload-image-button"><FontAwesomeIcon icon={faUpload} /> Upload Image</button>
+                <input onClick={this.getFile} type="file" id="upload-file" className="hide" name="filename" accept=".png,.jpg,.jpeg,.jfif,.pjpeg,.pjp" />
                 <br />
                 <div className="start-stack-divs">
                     <div className="start-stacking-divs start-imgdiv-1">
