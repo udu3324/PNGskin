fe45ca5f485bf51a5257fc3ee73bb04b54b1e7f7
udu3324 <47045986+udu3324@users.noreply.github.com>
2022-10-04 18:44:18 -0500

    Add precise controls & save settings to cookies

--- src/Editor.js
+++ src/Editor.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faCircleCheck, faMaximize, faUpDownLeftRight } from '@fortawesome/free-solid-svg-icons'
-import { imageBase64, setImage } from ".";
+import { getCookie, imageBase64, setCookie, setImage } from ".";
 
 export let color = "#000000";
 
 export let wrap = false;
+
+var interval = 5
 
 let sx;
 let sy;
@@ -18,6 +20,8 @@ let dh;
 let openedAdvancedMenu = false;
 
 let scaleAspectRatio = false;
+
+let precise = false;
 
 //draws the new edited image to the canvas
 function sync() {
@@ -97,18 +101,50 @@ export function setCanvasImg() {
 }
 
 class Editor extends React.Component {
+    componentDidMount() {
+        if (getCookie("wrapimage") === "true") {
+            document.getElementById('wrap-input').checked = true
+            this.wrapInput()
+            console.log("wrap image has been automatically set")
+        }
+        if (getCookie("aspectratio") === "true") {
+            document.getElementById('aspect-ratio-input').checked = true
+            this.aspectRatio()
+            console.log("aspect ratio lock has been automatically set")
+        }
+        if (getCookie("preciseedit") === "true") {
+            document.getElementById('precise-editing-input').checked = true
+            this.preciseEditing()
+            console.log("precise edit has been automatically set")
+        }
+    }
+
     constructor(props) {
         super(props);
 
         this.sx = this.sx.bind(this);
+        this.sxM = this.sxM.bind(this);
+        this.sxP = this.sxP.bind(this);
+
         this.sy = this.sy.bind(this);
+        this.syM = this.syM.bind(this);
+        this.syP = this.syP.bind(this);
+
         this.sw = this.sw.bind(this);
+        this.swM = this.swM.bind(this);
+        this.swP = this.swP.bind(this);
+
         this.sh = this.sh.bind(this);
+        this.shM = this.shM.bind(this);
+        this.shP = this.shP.bind(this);
 
         this.openAdvanced = this.openAdvanced.bind(this);
         this.colorInput = this.colorInput.bind(this);
         this.wrapInput = this.wrapInput.bind(this);
         this.aspectRatio = this.aspectRatio.bind(this);
+        this.arM = this.arM.bind(this);
+        this.arP = this.arP.bind(this);
+        this.preciseEditing = this.preciseEditing.bind(this);
 
         this.finish = this.finish.bind(this);
     }
@@ -118,8 +154,32 @@ class Editor extends React.Component {
         sync()
     }
 
+    sxM() {
+        sx = parseInt(document.getElementById('sx').value) - interval
+        document.getElementById('sx').value = sx
+        sync()
+    }
+
+    sxP() {
+        sx = parseInt(document.getElementById('sx').value) + interval
+        document.getElementById('sx').value = sx
+        sync()
+    }
+
     sy() {
         sy = document.getElementById('sy').value
+        sync()
+    }
+
+    syM() {
+        sy = parseInt(document.getElementById('sy').value) - interval
+        document.getElementById('sy').value = sy
+        sync()
+    }
+
+    syP() {
+        sy = parseInt(document.getElementById('sy').value) + interval
+        document.getElementById('sy').value = sy
         sync()
     }
 
@@ -128,8 +188,32 @@ class Editor extends React.Component {
         sync()
     }
 
+    swM() {
+        sw = parseInt(document.getElementById('sw').value) - interval
+        document.getElementById('sw').value = sw
+        sync()
+    }
+
+    swP() {
+        sw = parseInt(document.getElementById('sw').value) + interval
+        document.getElementById('sw').value = sw
+        sync()
+    }
+
     sh() {
         sh = document.getElementById('sh').value
+        sync()
+    }
+
+    shM() {
+        sh = parseInt(document.getElementById('sh').value) - interval
+        document.getElementById('sh').value = sh
+        sync()
+    }
+
+    shP() {
+        sh = parseInt(document.getElementById('sh').value) + interval
+        document.getElementById('sh').value = sh
         sync()
     }
 
@@ -158,18 +242,72 @@ class Editor extends React.Component {
     }
 
     wrapInput() {
-        wrap = document.getElementById('wrap-input').checked
+        if (wrap) {
+            setCookie("wrapimage", "false", 9999999999)
+            wrap = false;
+        } else {
+            setCookie("wrapimage", "true", 9999999999)
+            wrap = true;
+        }
     }
 
     aspectRatio() {
         if (scaleAspectRatio) {
+            setCookie("aspectratio", "false", 9999999999)
             scaleAspectRatio = false;
+
             document.getElementById('editor-size-control-div').style.display = "block"
             document.getElementById('editor-aspect-ratio-size-control-div').style.display = "none"
         } else {
+            setCookie("aspectratio", "true", 9999999999)
             scaleAspectRatio = true;
+
             document.getElementById('editor-size-control-div').style.display = "none"
             document.getElementById('editor-aspect-ratio-size-control-div').style.display = "block"
+        }
+    }
+
+    arM() {
+        sh = ((parseInt(document.getElementById('size-aspect-ratio').value) - interval) * sh) / sw
+        sw = parseInt(document.getElementById('size-aspect-ratio').value) - interval
+
+        document.getElementById('size-aspect-ratio').value = sw
+
+        document.getElementById('sw').value = sw
+        document.getElementById('sh').value = sh
+        sync()
+    }
+
+    arP() {
+        sh = ((parseInt(document.getElementById('size-aspect-ratio').value) + interval) * sh) / sw
+        sw = parseInt(document.getElementById('size-aspect-ratio').value) + interval
+
+        document.getElementById('size-aspect-ratio').value = sw
+
+        document.getElementById('sw').value = sw
+        document.getElementById('sh').value = sh
+        sync()
+    }
+
+    preciseEditing() {
+        if (precise) {
+            setCookie("preciseedit", "false", 9999999999)
+            precise = false;
+
+            document.getElementById('precise-div-1').style.display = "none"
+            document.getElementById('precise-div-2').style.display = "none"
+            document.getElementById('precise-div-3').style.display = "none"
+            document.getElementById('precise-div-4').style.display = "none"
+            document.getElementById('precise-div-5').style.display = "none"
+        } else {
+            setCookie("preciseedit", "true", 9999999999)
+            precise = true;
+
+            document.getElementById('precise-div-1').style.display = "block"
+            document.getElementById('precise-div-2').style.display = "block"
+            document.getElementById('precise-div-3').style.display = "block"
+            document.getElementById('precise-div-4').style.display = "block"
+            document.getElementById('precise-div-5').style.display = "block"
         }
     }
 
@@ -196,34 +334,54 @@ class Editor extends React.Component {
                 <div className="editor-grid-left">
                     <div>
                         <span className="editor-input-label"><FontAwesomeIcon icon={faUpDownLeftRight} /> Move Left & Right</span>
-                        <br />
                         <input id="sx" className="editor-range-input" onChange={this.sx} type="range" min="1" max="100"></input>
+
+                        <div id="precise-div-1" className="editor-precise-div">
+                            <button onClick={this.sxM} className="editor-precise-button">-</button>
+                            <button onClick={this.sxP} className="editor-precise-button editor-precise-left-button">+</button>
+                        </div>
                     </div>
 
                     <div>
                         <span className="editor-input-label"><FontAwesomeIcon icon={faUpDownLeftRight} /> Move Up & Down</span>
-                        <br />
                         <input id="sy" className="editor-range-input" onChange={this.sy} type="range" min="1" max="100"></input>
+
+                        <div id="precise-div-2" className="editor-precise-div">
+                            <button onClick={this.syM} className="editor-precise-button">-</button>
+                            <button onClick={this.syP} className="editor-precise-button editor-precise-left-button">+</button>
+                        </div>
                     </div>
 
                     <div id="editor-size-control-div">
                         <div>
                             <span className="editor-input-label"><FontAwesomeIcon icon={faMaximize} /> Size Left & Right</span>
-                            <br />
                             <input id="sw" className="editor-range-input invert-input" onChange={this.sw} type="range" min="1" max="100"></input>
+
+                            <div id="precise-div-3" className="editor-precise-div">
+                                <button onClick={this.swP} className="editor-precise-button">-</button>
+                                <button onClick={this.swM} className="editor-precise-button editor-precise-left-button">+</button>
+                            </div>
                         </div>
 
                         <div>
                             <span className="editor-input-label"><FontAwesomeIcon icon={faMaximize} /> Size Up & Down</span>
-                            <br />
                             <input id="sh" className="editor-range-input invert-input" onChange={this.sh} type="range" min="1" max="100"></input>
+
+                            <div id="precise-div-4" className="editor-precise-div">
+                                <button onClick={this.shP} className="editor-precise-button">-</button>
+                                <button onClick={this.shM} className="editor-precise-button editor-precise-left-button">+</button>
+                            </div>
                         </div>
                     </div>
 
                     <div id="editor-aspect-ratio-size-control-div" className="editor-aspect-ratio-size-control-div">
                         <span className="editor-input-label"><FontAwesomeIcon icon={faMaximize} /> Size Aspect Ratio</span>
-                        <br />
                         <input id="size-aspect-ratio" className="editor-range-input invert-input" onChange={this.sizeAspectRatio} type="range" min="1" max="100"></input>
+
+                        <div id="precise-div-5" className="editor-precise-div">
+                            <button onClick={this.arP} className="editor-precise-button">-</button>
+                            <button onClick={this.arM} className="editor-precise-button editor-precise-left-button">+</button>
+                        </div>
                     </div>
 
                     <div className="advanced-dropdown-bg">
@@ -243,6 +401,11 @@ class Editor extends React.Component {
                             <div>
                                 <input onClick={this.aspectRatio} type="checkbox" id="aspect-ratio-input" className="aspect-ratio-input" />
                                 <span className="editor-input-label"> Scale to Aspect Ratio</span>
+                            </div>
+
+                            <div>
+                                <input onClick={this.preciseEditing} type="checkbox" id="precise-editing-input" className="precise-editing-input" />
+                                <span className="editor-input-label"> Precise Editing (5px)</span>
                             </div>
                         </div>
                     </div>
--- src/index.js
+++ src/index.js
@@ -37,6 +37,33 @@ export function setImage(base64) {
   };
 }
 
+//cookies:
+//wrapimage|bool string
+//aspectratio|bool string
+//preciseedit|bool string
+
+export function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+export function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0)
+          return c.substring(name.length, c.length);
+  }
+  return "";
+}
+
 console.log("pngskin by udu3324 (https://github.com/udu3324)")
 
 ReactDOM.render(
